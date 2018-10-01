f5d73351663a56e95c5309f3f11ffab32004e603
triricam <mumin.irican@eu.sony.com>
2018-10-01 14:55:26 +0300

    commit List Trait

--- src/main/scala/week_1/List.scala
+++ src/main/scala/week_1/List.scala
@@ -1,5 +1,26 @@
 package week_1
 
-trait List {
+import java.util.NoSuchElementException
+
+trait List[T] {
+  def isEmpty: Boolean
+
+  def head: T
+
+  def tail: List[T]
+  //def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty: Boolean = true
+
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 
 }
+
